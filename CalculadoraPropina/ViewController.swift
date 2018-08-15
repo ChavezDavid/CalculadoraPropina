7758afc73531a3141a01600f039371b92348265f
Maestro <Maestro@iMac-de-Maestro-2.local>
2018-08-14 18:36:55 -0700

    A ver *2

--- CalculadoraPropina/ViewController.swift
+++ CalculadoraPropina/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var porcentaje = 0.0
+    var precio = 0.0
+    var subtotal = 0.0
+    var total = 0.0
+    
+    @IBOutlet weak var sldPorcentaje: UISlider!
+    @IBOutlet weak var lblPropina: UILabel!
+    @IBOutlet weak var txtPrecio: UITextField!
+    @IBOutlet weak var lblSubtotal: UILabel!
+    @IBOutlet weak var lblTotal: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func doSlidePorcentaje(sender: AnyObject) {
+        porcentaje = Double(sldPorcentaje.value)
+        let precio = Double(txtPrecio.text!)
+        subtotal = precio! * porcentaje / 100
+        
+        lblPropina.text = String(porcentaje)
+        lblSubtotal.text = String(subtotal)
+        lblTotal.text = String(precio! + subtotal)
+    }
 
 }
 
